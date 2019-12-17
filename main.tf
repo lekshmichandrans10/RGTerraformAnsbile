1530bf6c63a49d9268f3f8fe3be576dace3572bb
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2019-12-17 17:44:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.38.0"
 
-  subscription_id = 'AZURE_SUBSCRIPTION_ID'
-  tenant_id       = 'AZURE_TENANT_ID'
+  subscription_id = "$AZURE_SUBSCRIPTION_ID"
+  tenant_id       = "$AZURE_TENANT_ID"
 }
 
 /************ Resources Azure *********************/
